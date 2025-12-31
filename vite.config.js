81b3294223b8155d934c4396eb603453e01f2f55
sanika1971 <sanikagaikwad189@gmail.com>
2025-12-31 19:47:18 +0530

    fix netlify not found issue

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5174,  // Change from 3000 to 5174
-    open: true   // This will automatically open browser
-  }
-})
+  base: "/"
+});
